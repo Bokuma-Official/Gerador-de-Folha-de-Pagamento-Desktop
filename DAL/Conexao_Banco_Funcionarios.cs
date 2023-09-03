e6468916af3ee4f9fdad746752f579c6a373796e
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-09-03 05:00:36 -0300

    Nova conexão do banco de dados.

--- DAL/Conexao_Banco_Funcionarios.cs
+++ DAL/Conexao_Banco_Funcionarios.cs
@@ -12,7 +12,8 @@ namespace Gerador_de_Folha_de_Pagamento_Desktop.DAL
         {
             get
             {
-                return $"Data Source=bokuma.c5x1dupdjcvc.us-east-2.rds.amazonaws.com,1433;Initial Catalog=Funcionarios_Ataron;Persist Security Info=True;User ID=bokumatm;Password=bokumatm";
+                return $"Data Source=MATHEUS-C5\\SQLEXPRESS;Initial Catalog=Funcionarios_Ataron;" +
+                    $"Persist Security Info=True;User ID=bokumatm;Password=bokumatm";
             }
         }
     }
