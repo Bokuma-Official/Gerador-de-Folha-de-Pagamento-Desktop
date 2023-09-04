d84625df8d81b07a2b9f654fd2d4a2258de4ce6e
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-09-03 23:28:04 -0300

    Mudança de conexão do banco de dados.

--- DAL/Conexao_Banco_Folha.cs
+++ DAL/Conexao_Banco_Folha.cs
@@ -12,8 +12,8 @@ namespace Gerador_de_Folha_de_Pagamento_Desktop.DAL
         {
             get
             {
-                return $"Data Source=MATHEUS-C5\\SQLEXPRESS;Initial Catalog=Folha_Pagamento_Ataron;" +
-                    $"Persist Security Info=True;User ID=bokumatm;Password=bokumatm";
+                return $"Data Source=bokumatm.database.windows.net;Initial Catalog=Folha_Pagamento_Ataron;" +
+                    $"Persist Security Info=True;User ID=bokumatm;Password=BkmFPRH@9";
             }
         }
     }
